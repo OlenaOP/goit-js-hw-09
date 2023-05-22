17e853b34378204a1815400cb624fa3660ec5c8e
OlenaOP <elena.peredriy@gmail.com>
2023-05-22 11:29:30 +0300

    change package.json and tasks

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+buttonStop.disabled = true;
+const body = document.querySelector('body');
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
